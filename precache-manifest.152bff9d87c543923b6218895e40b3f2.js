707e8ea8a0e13fedecd7477fd9d2886b9fa60b1f
tamara <tamarakozok@gmail.com>
2021-08-07 15:14:56 +0300

    Updates

---
+++ precache-manifest.152bff9d87c543923b6218895e40b3f2.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b26f153166c0e896cb1adf97766b71f",
+    "url": "/react-robots-search/index.html"
+  },
+  {
+    "revision": "4eb317e4822cc64ec356",
+    "url": "/react-robots-search/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "74a266f48ec49cdf1607",
+    "url": "/react-robots-search/static/css/main.f19f1d4f.chunk.css"
+  },
+  {
+    "revision": "4eb317e4822cc64ec356",
+    "url": "/react-robots-search/static/js/2.1d06bf1d.chunk.js"
+  },
+  {
+    "revision": "7fc60fe721fa046663bd312b1d917655",
+    "url": "/react-robots-search/static/js/2.1d06bf1d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "74a266f48ec49cdf1607",
+    "url": "/react-robots-search/static/js/main.04d3fa09.chunk.js"
+  },
+  {
+    "revision": "479009b8da02484dfc54",
+    "url": "/react-robots-search/static/js/runtime-main.eca9702f.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/react-robots-search/static/media/SEGA.12a3db8b.woff"
+  }
+]);
